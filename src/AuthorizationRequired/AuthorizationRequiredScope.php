0b86e540626ff2b7d66cd4286824314ec01a22b3
Otto <ottorottier@gmail.com>
2015-08-21 20:25:29 +0200

    AuthorizationRequiredScope implements ScopeInterface

--- src/AuthorizationRequired/AuthorizationRequiredScope.php
+++ src/AuthorizationRequired/AuthorizationRequiredScope.php
@@ -3,7 +3,18 @@
 namespace AuthorizationRequired;
 
 use Illuminate\Database\Eloquent\ScopeInterface;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 
 class AuthorizationRequiredScope implements ScopeInterface
 {
+	public function apply(Builder $builder, Model $model)
+	{
+		$model::authorizationReadScope($builder);
+	}
+
+	public function remove(Builder $builder, Model $model)
+	{
+		//
+	}
 }
